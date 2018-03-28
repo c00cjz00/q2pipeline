a78724e46d8c6cc773d484dd1765a70899a0de0e
peterleary <35565521+peterleary@users.noreply.github.com>
2018-03-28 11:18:17 +0100

    Update deblur.sh

--- deblur.sh
+++ deblur.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 #QIIME 2 pipeline by Peter Leary 
 #Step 2.4 - Deblur 
-mkdir ~/Desktop/qiime2/$name/$sv/
+mkdir ../$name/$sv/
 #
 qiime quality-filter q-score \
- --i-demux ~/Desktop/qiime2/$name/demux/demux.qza \
- --o-filtered-sequences ~/Desktop/qiime2/$name/$sv/demux-filtered.qza \
- --o-filter-stats ~/Desktop/qiime2/$name/$sv/demux-filter-stats.qza
+ --i-demux ../$name/demux/demux.qza \
+ --o-filtered-sequences ../$name/$sv/demux-filtered.qza \
+ --o-filter-stats ../$name/$sv/demux-filter-stats.qza
  #
  qiime deblur denoise-16S \
-  --i-demultiplexed-seqs ~/Desktop/qiime2/$name/$sv/demux-filtered.qza \
+  --i-demultiplexed-seqs ../$name/$sv/demux-filtered.qza \
   --p-trim-length $trunclen \
-  --o-representative-sequences ~/Desktop/qiime2/$name/$sv/rep-seqs.qza \
-  --o-table ~/Desktop/qiime2/$name/$sv/table.qza \
-  --o-stats ~/Desktop/qiime2/$name/$sv/deblur.qza
+  --o-representative-sequences ../$name/$sv/rep-seqs.qza \
+  --o-table ../$name/$sv/table.qza \
+  --o-stats ../$name/$sv/deblur.qza
   #
   #
 qiime feature-table summarize \
-  --i-table $HOME/Desktop/QIIME2/$name/$sv/table.qza \
-  --o-visualization $HOME/Desktop/QIIME2/$name/$sv/table.qzv \
-  --m-sample-metadata-file $HOME/Desktop/QIIME2/$name/"$name"map.txt \
+  --i-table ../$name/$sv/table.qza \
+  --o-visualization ../$name/$sv/table.qzv \
+  --m-sample-metadata-file ../$name/"$name"map.txt \
   --quiet
 #
 qiime feature-table tabulate-seqs \
-  --i-data $HOME/Desktop/QIIME2/$name/$sv/rep-seqs.qza \
-  --o-visualization $HOME/Desktop/QIIME2/$name/$sv/rep-seqs.qzv \
+  --i-data ../$name/$sv/rep-seqs.qza \
+  --o-visualization ../$name/$sv/rep-seqs.qzv \
   --quiet
 #
-qiime tools export $HOME/Desktop/QIIME2/$name/$sv/table.qzv --output-dir $HOME/Desktop/QIIME2/$name/$sv
+qiime tools export ../$name/$sv/table.qzv --output-dir ../$name/useful/table
 #
-qiime tools export $HOME/Desktop/QIIME2/$name/$sv/rep-seqs.qzv --output-dir $HOME/Desktop/QIIME2/$name/$sv
+qiime tools export ../$name/$sv/table.qza --output-dir ../$name/useful/biomtable
+#
+qiime tools export ..$name/$sv/rep-seqs.qzv --output-dir ../$name/useful/rep-seqs
+#
+cp ../$name/useful/rep-seqs/sequences.fasta ../$name/useful/rep-seqs/rep-seqs.txt
