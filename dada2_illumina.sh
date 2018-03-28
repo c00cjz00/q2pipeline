5edb57f781937d2109bf54288a9efc0b8d4b2c5f
peterleary <35565521+peterleary@users.noreply.github.com>
2018-03-28 10:52:05 +0100

    Update dada2_illumina.sh

--- dada2_illumina.sh
+++ dada2_illumina.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 #Peter Leary
-mkdir $HOME/Desktop/qiime2/$name/dada2
+mkdir ../$name/dada2
 qiime dada2 denoise-paired \
-  --i-demultiplexed-seqs ~/Desktop/QIIME2/$name/demux/demux.qza \
-  --o-table ~/Desktop/qiime2/$name/dada2/table \
-  --o-representative-sequences ~/Desktop/qiime2/$name/dada2/rep-seqs \
+  --i-demultiplexed-seqs ../$name/demux/demux.qza \
+  --o-table ../$name/dada2/table \
+  --o-representative-sequences ../$name/dada2/rep-seqs \
   --p-trim-left-f $trimleft \
   --p-trim-left-r $trimleft \
   --p-trunc-len-f $trunclen \
-  --p-trunc-len-r 180 \
+  --p-trunc-len-r $truncrev \
   --verbose \
   --p-n-threads 0
 #
 qiime feature-table summarize \
-  --i-table $HOME/Desktop/QIIME2/$name/$sv/table.qza \
-  --o-visualization $HOME/Desktop/QIIME2/$name/$sv/table.qzv \
-  --m-sample-metadata-file $HOME/Desktop/QIIME2/$name/"$name"map.txt \
+  --i-table ../$name/$sv/table.qza \
+  --o-visualization ../$name/$sv/table.qzv \
+  --m-sample-metadata-file ../$name/"$name"map.txt \
   --quiet
 #
 qiime feature-table tabulate-seqs \
-  --i-data $HOME/Desktop/QIIME2/$name/$sv/rep-seqs.qza \
-  --o-visualization $HOME/Desktop/QIIME2/$name/$sv/rep-seqs.qzv \
+  --i-data ../$name/$sv/rep-seqs.qza \
+  --o-visualization ../$name/$sv/rep-seqs.qzv \
   --quiet 
 #
-qiime tools export $HOME/Desktop/QIIME2/$name/$sv/table.qzv --output-dir $HOME/Desktop/QIIME2/$name/useful/table
+qiime tools export ../$name/$sv/table.qzv --output-dir ../$name/useful/table
 #
-qiime tools export $HOME/Desktop/QIIME2/$name/$sv/table.qza --output-dir $HOME/Desktop/QIIME2/$name/useful/biomtable
+qiime tools export ../$name/$sv/table.qza --output-dir ../$name/useful/biomtable
 #
-qiime tools export $HOME/Desktop/QIIME2/$name/$sv/rep-seqs.qzv --output-dir $HOME/Desktop/QIIME2/$name/useful/rep-seqs
-cp $HOME/Desktop/QIIME2/$name/useful/rep-seqs/sequences.fasta $HOME/Desktop/QIIME2/$name/useful/rep-seqs/rep-seqs.txt
+qiime tools export ..$name/$sv/rep-seqs.qzv --output-dir ../$name/useful/rep-seqs
+#
+cp ../$name/useful/rep-seqs/sequences.fasta ../$name/useful/rep-seqs/rep-seqs.txt
