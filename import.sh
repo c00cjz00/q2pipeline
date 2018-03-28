77e093ae3fc8d33e325dea0a44d83feb91740831
peterleary <35565521+peterleary@users.noreply.github.com>
2018-03-28 12:26:36 +0100

    Delete import.sh

--- import.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#QIIME 2 Ion Torrent Pipeline by Peter Leary 
-#Step 2.1 - importing EMP-protocol data 
-qiime tools import \
-  --type EMPSingleEndSequences \
-  --input-path ../$name/emp \
-  --output-path ../$name/emp/emp-single-end-sequences.qza
