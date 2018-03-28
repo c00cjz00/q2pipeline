964a27d0d901a5cf7f92e43b16c56161a6bcb41b
peterleary <35565521+peterleary@users.noreply.github.com>
2018-03-28 11:48:02 +0100

    Update final.sh

--- final.sh
+++ final.sh
@@ -3,19 +3,19 @@
 #Step 2.11 - Tidying up 
 #
 #Moving alpha and beta diversity analyses 
-qiime tools export $HOME/Desktop/QIIME2/$name/diversity/core-metrics-results/faith-pd-group-significance.qzv --output-dir $HOME/Desktop/QIIME2/$name/diversity/core-metrics-results/faith-pd
+qiime tools export ../$name/diversity/core-metrics-results/faith-pd-group-significance.qzv --output-dir ../$name/diversity/core-metrics-results/faith-pd
 #
-qiime tools export $HOME/Desktop/QIIME2/$name/diversity/core-metrics-results/evenness-group-significance.qzv --output-dir $HOME/Desktop/QIIME2/$name/diversity/core-metrics-results/evenness 
+qiime tools export ../$name/diversity/core-metrics-results/evenness-group-significance.qzv --output-dir ../$name/diversity/core-metrics-results/evenness 
 #
-qiime tools export $HOME/Desktop/QIIME2/$name/diversity/core-metrics-results/unweighted-unifrac-emperor.qzv --output-dir $HOME/Desktop/QIIME2/$name/diversity/core-metrics-results/unweighted-unifrac
+qiime tools export ../$name/diversity/core-metrics-results/unweighted-unifrac-emperor.qzv --output-dir ../$name/diversity/core-metrics-results/unweighted-unifrac
 #
-qiime tools export $HOME/Desktop/QIIME2/$name/diversity/core-metrics-results/bray-curtis-emperor.qzv --output-dir $HOME/Desktop/QIIME2/$name/diversity/core-metrics-results/bray-curtis-emperor
+qiime tools export ../$name/diversity/core-metrics-results/bray-curtis-emperor.qzv --output-dir ../$name/diversity/core-metrics-results/bray-curtis-emperor
 #
-cp -a $HOME/Desktop/QIIME2/$name/diversity/core-metrics-results/faith-pd/. $HOME/Desktop/QIIME2/$name/useful/alpha-div/faith-pd
+cp -a ../$name/diversity/core-metrics-results/faith-pd/. ../$name/useful/alpha-div/faith-pd
 #
-cp -a $HOME/Desktop/QIIME2/$name/diversity/core-metrics-results/evenness/. $HOME/Desktop/QIIME2/$name/useful/alpha-div/eveness
+cp -a ../$name/diversity/core-metrics-results/evenness/. ../$name/useful/alpha-div/eveness
 #
-cp -a $HOME/Desktop/QIIME2/$name/diversity/core-metrics-results/unweighted-unifrac/. $HOME/Desktop/QIIME2/$name/useful/beta-div/unweighted-unifrac 
+cp -a ../$name/diversity/core-metrics-results/unweighted-unifrac/. ../$name/useful/beta-div/unweighted-unifrac 
 #
-cp -a $HOME/Desktop/QIIME2/$name/diversity/core-metrics-results/bray-curtis-emperor/. $HOME/Desktop/QIIME2/$name/useful/beta-div/bray-curtis
+cp -a ../$name/diversity/core-metrics-results/bray-curtis-emperor/. ../$name/useful/beta-div/bray-curtis
 #
