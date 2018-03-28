94baa20085a0bb9a9512e8fcfa67e5487f820fbe
peterleary <35565521+peterleary@users.noreply.github.com>
2018-03-28 10:30:57 +0100

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -16,17 +16,18 @@ if [ "${#1}" -ge "8" ]; then
 fi
 #
 #If the fastq file in the folder isn't the same name as you tell it it is, it will tell you off 
-if [ ! -f ~/Desktop/QIIME2/$1/"$1".fastq ]; then
+if [ ! -f ../$1/"$1".fastq ]; then
 	echo -e "FastQ file not found in ~/Desktop/qiime2/$1\n"
 	exit
 fi
 #
 echo "Okay, doing step 1. Give me 5 minutes."
-extract_barcodes.py -f $HOME/Desktop/QIIME2/$1/$1.fastq -o ~/Desktop/QIIME2/$1/emp -l 15 -c barcode_single_end -m ~/Desktop/QIIME2/$1/"$1"map.txt
+extract_barcodes.py -f ../$1/$1.fastq -o ../$1/emp -l 15 -c barcode_single_end -m ../$1/"$1"map.txt
 #Then to rename reads.fastq to sequences.fastq as per EMP protocol
-mv $HOME/Desktop/QIIME2/$1/emp/reads.fastq $HOME/Desktop/QIIME2/$1/emp/sequences.fastq
+mv ../$1/emp/reads.fastq ../$1/emp/sequences.fastq
 #Then to gzip both new files as per EMP protocol
-gzip $HOME/Desktop/QIIME2/$1/emp/sequences.fastq
-gzip $HOME/Desktop/QIIME2/$1/emp/barcodes.fastq
+gzip ../$1/emp/sequences.fastq
+gzip ../$1/emp/barcodes.fastq
 #
 echo "Okay, step 1 finished. Now enter the command for step 2!"
+source deactivate
