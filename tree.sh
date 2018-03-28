131bd691360686988e3683a05e67e433fb73d1d4
peterleary <35565521+peterleary@users.noreply.github.com>
2018-03-28 11:39:26 +0100

    Update tree.sh

--- tree.sh
+++ tree.sh
@@ -2,12 +2,12 @@
 #QIIME 2 Ion Torrent Pipeline by Peter Leary
 #Step 2.7 - Creating unrooted and midrooted phylogenetic trees 
 qiime phylogeny fasttree \
-  --i-alignment $HOME/Desktop/QIIME2/$name/aligned/masked-aligned-rep-seqs.qza \
-  --o-tree $HOME/Desktop/QIIME2/$name/aligned/unrooted-tree.qza \
+  --i-alignment ../$name/aligned/masked-aligned-rep-seqs.qza \
+  --o-tree ../$name/aligned/unrooted-tree.qza \
   --quiet
 #
 qiime phylogeny midpoint-root \
-  --i-tree $HOME/Desktop/QIIME2/$name/aligned/unrooted-tree.qza \
-  --o-rooted-tree $HOME/Desktop/QIIME2/$name/aligned/rooted-tree.qza \
+  --i-tree ../$name/aligned/unrooted-tree.qza \
+  --o-rooted-tree ../$name/aligned/rooted-tree.qza \
   --quiet
 #
