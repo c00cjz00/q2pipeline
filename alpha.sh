2415b1d0be5bd4540285f045fcd1144ce953a228
peterleary <35565521+peterleary@users.noreply.github.com>
2018-03-28 11:45:21 +0100

    Update alpha.sh

--- alpha.sh
+++ alpha.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 #QIIME 2 Ion Torrent Pipeline by Peter Leary
 #Step 2.9 - Alpha diversity
-mkdir $HOME/Desktop/QIIME2/$name/diversity
-mkdir $HOME/Desktop/QIIME2/$name/useful/alpha-div
-mkdir $HOME/Desktop/QIIME2/$name/useful/beta-div
+mkdir ../$name/diversity
+mkdir ../$name/useful/alpha-div
+mkdir ../$name/useful/beta-div
+#
 qiime diversity core-metrics-phylogenetic \
-  --i-phylogeny $HOME/Desktop/QIIME2/$name/aligned/rooted-tree.qza \
-  --i-table $HOME/Desktop/QIIME2/$name/$sv/table.qza \
+  --i-phylogeny ../$name/aligned/rooted-tree.qza \
+  --i-table ../$name/$sv/table.qza \
   --p-sampling-depth $sam \
-  --m-metadata-file $HOME/Desktop/QIIME2/$name/"$name"map.txt \
-  --output-dir $HOME/Desktop/QIIME2/$name/diversity/core-metrics-results \
+  --m-metadata-file ../$name/"$name"map.txt \
+  --output-dir ../$name/diversity/core-metrics-results \
   --quiet
 #
 qiime diversity alpha-group-significance \
-  --i-alpha-diversity $HOME/Desktop/QIIME2/$name/diversity/core-metrics-results/faith_pd_vector.qza \
-  --m-metadata-file $HOME/Desktop/QIIME2/$name/"$name"map.txt \
-  --o-visualization $HOME/Desktop/QIIME2/$name/diversity/core-metrics-results/faith-pd-group-significance.qzv \
+  --i-alpha-diversity ../$name/diversity/core-metrics-results/faith_pd_vector.qza \
+  --m-metadata-file ../$name/"$name"map.txt \
+  --o-visualization ../$name/diversity/core-metrics-results/faith-pd-group-significance.qzv \
   --quiet
 #
 qiime diversity alpha-group-significance \
-  --i-alpha-diversity $HOME/Desktop/QIIME2/$name/diversity/core-metrics-results/evenness_vector.qza \
-  --m-metadata-file $HOME/Desktop/QIIME2/$name/"$name"map.txt \
-  --o-visualization $HOME/Desktop/QIIME2/$name/diversity/core-metrics-results/evenness-group-significance.qzv\
+  --i-alpha-diversity ../$name/diversity/core-metrics-results/evenness_vector.qza \
+  --m-metadata-file ../$name/"$name"map.txt \
+  --o-visualization ../$name/diversity/core-metrics-results/evenness-group-significance.qzv\
   --quiet
 #
 qiime diversity alpha-rarefaction \
-  --i-table $HOME/Desktop/QIIME2/$name/$sv/table.qza \
-  --i-phylogeny $HOME/Desktop/QIIME2/$name/aligned/rooted-tree.qza \
+  --i-table ../$name/$sv/table.qza \
+  --i-phylogeny ../$name/aligned/rooted-tree.qza \
   --p-max-depth $sam \
-  --m-metadata-file $HOME/Desktop/QIIME2/$name/"$name"map.txt \
-  --o-visualization $HOME/Desktop/QIIME2/$name/diversity/alpha-rarefaction.qzv
+  --m-metadata-file ../$name/"$name"map.txt \
+  --o-visualization ../$name/diversity/alpha-rarefaction.qzv
 #
-qiime tools export $HOME/Desktop/QIIME2/$name/diversity/alpha-rarefaction.qzv --output-dir $HOME/Desktop/QIIME2/$name/useful/alpha-div/alpha-rarefaction
+qiime tools export ../$name/diversity/alpha-rarefaction.qzv --output-dir ../$name/useful/alpha-div/alpha-rarefaction
