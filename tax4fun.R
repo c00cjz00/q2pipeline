7f001df0380e5f37ee7e734c15dc7325811f3b4f
Peter Leary <35565521+peterleary@users.noreply.github.com>
2018-04-05 14:46:47 +0100

    Update tax4fun.R

--- tax4fun.R
+++ tax4fun.R
@@ -2,19 +2,17 @@
 
 # A script that will hopefully automate Tax4Fun 
 
-cat(R.version$version.string, "\n")
-args <- commandArgs(TRUE)
 
 library(Tax4Fun)
-setwd("/")
+getwd()
 
-files = c("$name/useful/closed/feature-table-tax4fun.txt")
+files = c("useful/closed/feature-table-tax4fun.txt")
 QIIMEData = importQIIMEData(files)
 
-pathReferenceData = "classifiers/tax4fun/SILVA119"
+pathReferenceData = "../classifiers/tax4fun/SILVA119"
 
 Tax4FunOutput <- Tax4Fun(QIIMEData,pathReferenceData, fctProfiling = TRUE, refProfile = "UProC", shortReadMode = TRUE, normCopyNo = TRUE)
 
 Tax4FunProfile <- data.frame(t(Tax4FunOutput$Tax4FunProfile))
 
-write.table(Tax4FunProfile,"$name/useful/tax4fun/Tax4FunProfile_Export.csv",sep="\t")
+write.table(Tax4FunProfile,"useful/tax4fun/Tax4FunProfile_Export.csv",sep="\t")
