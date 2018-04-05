fc4c21bb2b9ef819e16b15df254b788b9046666a
Peter Leary <35565521+peterleary@users.noreply.github.com>
2018-04-05 15:56:07 +0100

    Update install_tax4fun.R

--- install_tax4fun.R
+++ install_tax4fun.R
@@ -2,8 +2,9 @@
 
 # A script to install Tax4Fun and its dependencies... 
 
-install.packages('qiimer', repos='http://cran.us.r-project.org')
-install.packages('RJSONIO', repos='http://cran.us.r-project.org')
-install.packages('https://cran.r-project.org/src/contrib/Archive/biom/biom_0.3.12.tar.gz')
-install.packages('http://tax4fun.gobics.de/Tax4Fun/Tax4Fun_0.3.1.tar.gz')
-download.file('http://tax4fun.gobics.de/Tax4Fun/ReferenceData/SILVA119.zip', destfile="classifiers/tax4fun/SILVA119")
+install.packages('qiimer', repos='http://cran.us.r-project.org', quiet = TRUE)
+install.packages('RJSONIO', repos='http://cran.us.r-project.org', quiet = TRUE)
+install.packages('https://cran.r-project.org/src/contrib/Archive/biom/biom_0.3.12.tar.gz', quiet = TRUE)
+install.packages('http://tax4fun.gobics.de/Tax4Fun/Tax4Fun_0.3.1.tar.gz', quiet = TRUE)
+download.file('http://tax4fun.gobics.de/Tax4Fun/ReferenceData/SILVA119.zip', destfile="classifiers/tax4fun/SILVA119.zip", quiet = TRUE)
+unzip("classifiers/tax4fun/SILVA119.zip", exdir="classifiers/tax4fun/.")
