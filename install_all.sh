46cee728da37a0c7092cb4fa12eb99d67a8321bc
Peter Leary <35565521+peterleary@users.noreply.github.com>
2018-04-09 15:17:09 +0100

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -4,18 +4,20 @@
 
 if
 [[ "$(uname)" ==  "Darwin" ]]; then                                             # Checks to see if running macOS and then downloads the right Miniconda
-curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh 
-echo -e "\nPlease follow the instructions to install Miniconda.\n"
-bash Miniconda3-latest-MacOSX-x86_64.sh
+wget https://www.stats.bris.ac.uk/R/bin/macosx/R-3.4.4.pkg                      # Downloads R 3.4.4 from Bristol Uni 
+sudo installer -pkg R-3.4.4.pkg -target /                                       # Installs R to the Applications folder 
+rm *pkg
+curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh  # Downloads the Miniconda installer 
+bash Miniconda3-latest-MacOSX-x86_64.sh                                         # Executes the Miniconda installer 
 rm -rf Miniconda3-latest-MacOSX-x86_64.sh                                       # Deletes the Miniconda file to be nice and tidy 
 #
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then                      # Checks to see if running Linux instead, and do the right thing 
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then                      # Checks to see if running Linux instead, and does as above for Linux 
+deb https://www.stats.bris.ac.uk/R/bin/linux/ubuntu/xenial/                     # Installs R for Ubuntu 16.04 Xenial Xerus – as this is the most recent LTS release at time of writing
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-echo -e "\nPlease follow the instructions to install Miniconda.\n"
 bash Miniconda3-latest-Linux-x86_64.sh
 rm -rf Miniconda3-latest-Linux-x86_64.sh
 fi
 #
 open -a Terminal.app install_qiime.sh                                           # Opens a new Terminal to install the two QIIMEs because Miniconda makes you close the Terminal upon installation 
 echo -e "\nQIIME1 and 2 are now installing in the other window. Once these have finished, either the window will close automatically or you will see [Process Completed], in which case, you can close the window.\nEither way, you will need to open a new Terminal window to continue setting up the pipeline."
-exit 
+exit 0
