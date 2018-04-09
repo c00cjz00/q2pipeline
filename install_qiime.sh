5bc24a9aea7ddeda987f89f48e4222fcbb7fe923
Peter Leary <35565521+peterleary@users.noreply.github.com>
2018-04-09 15:19:35 +0100

    Update install_qiime.sh

--- install_qiime.sh
+++ install_qiime.sh
@@ -2,6 +2,9 @@
 # Peter Leary 
 if
 [[ "$(uname)" ==  "Darwin" ]]; then                                             # Checks to see if running macOS and then downloads the right Miniconda
+wget https://www.stats.bris.ac.uk/R/bin/macosx/R-3.4.4.pkg                      # Downloads R 3.4.4 from Bristol Uni 
+sudo installer -pkg R-3.4.4.pkg -target /                                       # Installs R to the Applications folder 
+rm *pkg                                                                         # Delete R pkg to be tidy
 wget https://data.qiime2.org/distro/core/qiime2-2018.2-py35-osx-conda.yml       # Downloads the .yaml for QIIME2 
 conda env create -n qiime2-2018.2 --file qiime2-2018.2-py35-osx-conda.yml       # Creates the QIIME2 environment
 rm qiime2-2018.2-py35-osx-conda.yml                                             # Deletes the QIIME2 .yaml to be tidy 
