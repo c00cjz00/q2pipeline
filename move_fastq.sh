d7201efd9d0937c0157b52f3caba482e00294b35
Peter Leary <35565521+peterleary@users.noreply.github.com>
2018-05-07 10:57:21 +0100

    Create move_fastq.sh
    A script that moves Illumina fastqs from their subdirectories into one single directory, to make importing into QIIME2 easier.

---
+++ move_fastq.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Peter Leary 
+# A script to pull out Illumina fastq files from subdirectories into one folder, so QIIME2 can pull them into demux.
+# Requires a named folder, which contains a 'Seqs' folder, which contains all the folders containing Illumina reads 
+
+sourcedir=$1/Seqs/                # Tells it that the fastqs are in folders in the 'Seqs' folder
+destdir=$1/Seqs                   # Tells it to put the moved files just all into 'Seqs'
+
+for f in $sourcedir/*             # Looking for files in the 'Seqs/folder' folders 
+do
+    mv $f/*R1*.fastq.gz $destdir  # Move forward reads into 'Seqs'
+    mv $f/*R2*.fastq.gz $destdir  # More reverse reads into 'Seqs'
+done                              # Done
+rm -r $1/Seqs/*/                  # Delete the folders from whence the fastqs came, to be tidy 
