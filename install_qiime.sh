d545634af39f74e39740ead026d711fdd5a0e1e1
Peter Leary <35565521+peterleary@users.noreply.github.com>
2018-05-15 13:14:23 +0100

    Update install_qiime.sh

--- install_qiime.sh
+++ install_qiime.sh
@@ -11,6 +11,10 @@ rm qiime2-2018.2-py35-osx-conda.yml                                             
 conda create -n qiime1 python=2.7 qiime matplotlib=1.4.3 mock nose -c bioconda  # Installs QIIME1 too for good measure 
 #
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then                      # Checks to see if running Linux and does the same things as above 
+codename=$(lsb_release -c -s)                                                   # Checks which version of Ubuntu is being run
+deb https://www.stats.bris.ac.uk/R/bin/linux/ubuntu $codename/                  # Downloads appropriate version of R 
+sudo apt-get update 
+sudo apt-get install r-base 
 wget https://data.qiime2.org/distro/core/qiime2-2018.4-py35-linux-conda.yml
 conda env create -n qiime2-2018.4 --file qiime2-2018.4-py35-linux-conda.yml
 rm qiime2-2018.2-py35-linux-conda.yml
