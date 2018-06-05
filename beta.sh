b1ed22b415e1d6e8670871710ef4f8131e3249b4
Peter Leary <35565521+peterleary@users.noreply.github.com>
2018-06-05 15:41:45 +0100

    Update beta.sh
    Added weighted UniFrac in addition to Bray Curtis because they look more different than I expected and that's made me a little embarrassed

--- beta.sh
+++ beta.sh
@@ -9,6 +9,12 @@ qiime emperor plot \
   --o-visualization $name/diversity/core-metrics-results/unweighted-unifrac-emperor.qzv \
   --quiet
 #
+# Weighted UniFrac PCoA Plot 
+qiime emperor plot \
+  --i-pcoa $name/diversity/core-metrics-results/weighted_unifrac_pcoa_results.qza \
+  --m-metadata-file $name/"$name"map.txt \
+  --o-visualization $name/diversity/core-metrics-results/weighted-unifrac-emperor.qzv \
+  --quiet
 # Bray Curtis (weighted) PCoA Plot 
 qiime emperor plot \
   --i-pcoa $name/diversity/core-metrics-results/bray_curtis_pcoa_results.qza \
@@ -26,6 +32,14 @@ qiime diversity beta-group-significance \
  --quiet
 #
 qiime diversity beta-group-significance \
+ --i-distance-matrix $name/diversity/core-metrics-results/weighted_unifrac_distance_matrix.qza \
+ --m-metadata-file $name/"$name"map.txt \
+ --m-metadata-column Treatment \
+ --o-visualization $name/diversity/core-metrics-results/weighted_unifrac_treatment_significance.qzv \
+ --p-pairwise \
+ --quiet
+#
+qiime diversity beta-group-significance \
  --i-distance-matrix $name/diversity/core-metrics-results/bray_curtis_distance_matrix.qza \
  --m-metadata-file $name/"$name"map.txt \
  --m-metadata-column Treatment \
@@ -35,9 +49,8 @@ qiime diversity beta-group-significance \
 #
 # Export all figures 
 qiime tools export $name/diversity/core-metrics-results/unweighted-unifrac-emperor.qzv --output-dir $name/useful/beta-div/unweighted-unifrac-pcoa
-#
+qiime tools export $name/diversity/core-metrics-results/weighted-unifrac-emperor.qzv --output-dir $name/useful/beta-div/weighted-unifrac-pcoa
 qiime tools export $name/diversity/core-metrics-results/bray-curtis-emperor.qzv --output-dir $name/useful/beta-div/bray-curtis-pcoa
-#
 qiime tools export $name/diversity/core-metrics-results/unweighted_unifrac_treatment_significance.qzv --output-dir $name/useful/beta-div/unweighted-unifrac-significance
-#
+qiime tools export $name/diversity/core-metrics-results/weighted_unifrac_treatment_significance.qzv --output-dir $name/useful/beta-div/weighted-unifrac-significance
 qiime tools export $name/diversity/core-metrics-results/bray_curtis_treatment_significance.qzv --output-dir $name/useful/beta-div/bray-curtis-significance
