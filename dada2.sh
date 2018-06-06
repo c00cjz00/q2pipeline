d0554c9c79c399d9c9350b1c2231c825c2a9e4fd
Peter Leary <35565521+peterleary@users.noreply.github.com>
2018-06-06 22:47:03 +0100

    Update dada2.sh

--- dada2.sh
+++ dada2.sh
@@ -18,6 +18,12 @@ qiime feature-table summarize \
   --m-sample-metadata-file $name/"$name"map.txt \
   --quiet
 #
+qiime feature-table summarize \
+  --i-table $name/$sv/table.qza \
+  --o-visualization $name/$sv/table.qzv \
+  --m-sample-metadata-file $name/"$name"map.txt \
+  --quiet
+#
 qiime feature-table tabulate-seqs \
   --i-data $name/$sv/rep-seqs.qza \
   --o-visualization $name/$sv/rep-seqs.qzv \
