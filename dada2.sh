508148ee1368df7a14e12730e23f4102ac321efb
Peter Leary <35565521+peterleary@users.noreply.github.com>
2018-08-24 18:55:09 +0100

    Update dada2.sh

--- dada2.sh
+++ dada2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Peter Leary
 mkdir $name/$sv
+if [[ "$gene" == "16S" ]]; then
 qiime dada2 denoise-single \
   --i-demultiplexed-seqs $name/demux/demux.qza \
   --p-trim-left $trimleft \
@@ -11,6 +12,19 @@ qiime dada2 denoise-single \
   --p-n-threads $threads \
   --p-max-ee 5 \
   --quiet
+fi
+if [[ "$gene" == "ITS" ]]; then
+qiime dada2 denoise-single \
+  --i-demultiplexed-seqs $name/demux/demux.qza \
+  --p-trim-left $trimleft \
+  --p-trunc-len 0 \
+  --o-representative-sequences $name/$sv/rep-seqs.qza \
+  --o-table $name/$sv/table.qza \
+  --o-denoising-stats $name/$sv/stats.qza \
+  --p-n-threads $threads \
+  --p-max-ee 5 \
+  --quiet
+fi
 #
 qiime feature-table summarize \
   --i-table $name/$sv/table.qza \
