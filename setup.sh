ac55c338e0c1ef87dadb55870e3f470b0ba9ee80
Peter Leary <35565521+peterleary@users.noreply.github.com>
2018-08-24 18:55:36 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,9 +4,10 @@ mkdir scripts
 mv *sh scripts 
 mv *R scripts
 mkdir classifiers 
-mkdir classifiers/illumina
-mkdir classifiers/iontorrent
+mkdir classifiers/illumina_16S
+mkdir classifiers/iontorrent_16S
 mkdir classifiers/silva119
-mkdir classifiers/unite
+mkdir classifiers/illumina_ITS
+mkdir classifiders/iontorrent_ITS
 mkdir classifiers/tax4fun
 echo -e "\nFinished!\n"
