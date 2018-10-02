ba3828ab5c1287837a8ab3fda5622e99608efda1
Peter Leary <35565521+peterleary@users.noreply.github.com>
2018-10-02 14:11:18 +0100

    Update import_illumina.sh

--- import_illumina.sh
+++ import_illumina.sh
@@ -4,11 +4,11 @@ mkdir $name/demux
 qiime tools import \
   --type 'SampleData[PairedEndSequencesWithQuality]' \
   --input-path $name/seqs \
-  --source-format CasavaOneEightSingleLanePerSampleDirFmt \
+  --input-format CasavaOneEightSingleLanePerSampleDirFmt \
   --output-path $name/demux/demux.qza
 #
 qiime demux summarize \
 --i-data $name/demux/demux.qza \
 --o-visualization $name/demux/demux.qzv
 #
-qiime tools export $name/demux/demux.qzv --output-dir $name/demux/demux
+qiime tools export --input-path $name/demux/demux.qzv --output-path $name/demux/demux
