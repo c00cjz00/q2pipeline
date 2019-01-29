e56e9461358218ca27248aecd1b567bbe7a2e634
Peter Leary <peter.leary@me.com>
2019-01-29 14:29:00 +0000

    Extract aligned fastas and trees

--- align.sh
+++ align.sh
@@ -12,3 +12,13 @@ qiime alignment mask \
   --i-alignment $name/aligned/aligned-rep-seqs.qza \
   --o-masked-alignment $name/aligned/masked-aligned-rep-seqs.qza \
   --quiet
+
+mkdir $name/useful/rep-seqs-aligned
+
+qiime tools export --input-path $name/aligned/aligned-rep-seqs.qza --output-path $name/aligned/aligned-rep-seqs
+qiime tools export --input-path $name/aligned/masked-aligned-rep-seqs.qza --output-path $name/aligned/masked-aligned-rep-seqs
+
+cp $name/aligned/aligned-rep-seqs/aligned-dna-sequences.fasta $name/useful/rep-seqs-aligned
+cp $name/aligned/masked-aligned-rep-seqs/aligned-dna-sequences.fasta $name/useful/rep-seqs-aligned/masked-aligned-dna-sequences.fasta
+cp $name/useful/rep-seqs-aligned/aligned-dna-sequences.fasta $name/useful/rep-seqs-aligned/aligned-dna-sequences.txt
+cp $name/useful/rep-seqs-aligned/masked-aligned-dna-sequences.fasta $name/useful/rep-seqs-aligned/masked-aligned-dna-sequences.txt
--- tree.sh
+++ tree.sh
@@ -11,3 +11,10 @@ qiime phylogeny midpoint-root \
   --i-tree $name/aligned/unrooted-tree.qza \
   --o-rooted-tree $name/aligned/rooted-tree.qza \
   --quiet
+  
+qiime tools export --input-path $name/aligned/unrooted-tree.qza --output-path $name/aligned/unrooted-tree
+qiime tools export --input-path $name/aligned/rooted-tree.qza --output-path $name/aligned/rooted-tree
+
+mkdir $name/useful/trees 
+cp $name/aligned/unrooted-tree/tree.nwk $name/useful/trees/unrooted_tree.nwk 
+cp $name/aligned/rooted-tree/tree.nwk $name/useful/trees/rooted_tree.nwk 
