f9b835c4c266fd4846f463ffd640f5b12c9c9247
c00cjz00 <summerhill001@gmail.com>
2019-12-22 05:55:47 +0800

    Create install_rpkg_v1.r

---
+++ install_rpkg_v1.r
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+# example https://www.digitalocean.com/community/tutorials/how-to-install-r-packages-using-devtools-on-ubuntu-16-04
+# sudo apt-get install build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev
+# sudo -i R
+# install.packages('devtools')
+# sudo apt-get install -y r-cran-latticeextra
+install.packages("vegan")
+source("https://bioconductor.org/biocLite.R")
+biocLite("BiocInstaller")
+source("https://bioconductor.org/biocLite.R")
+biocLite("philr")
+source("https://bioconductor.org/biocLite.R")
+biocLite("DECIPHER")
+source("https://bioconductor.org/biocLite.R")
+biocLite("phyloseq")
+source("https://bioconductor.org/biocLite.R")
+biocLite("Biostrings")
+devtools::install_github("jbisanz/MicrobeR")
+devtools::install_github("jbisanz/qiime2R")
