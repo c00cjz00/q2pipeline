9272e7d70571250455d0c62ab3f00bd31b7beeb9
c00cjz00 <summerhill001@gmail.com>
2019-12-22 13:50:17 +0800

    Update install_jupyter_for_R.sh

--- install_jupyter_for_R.sh
+++ install_jupyter_for_R.sh
@@ -1,5 +1,20 @@
 
+#conda install -c r r-essentials
+sudo ln -s /bin/gzip /usr/bin/gzip
+sudo ln -s /bin/tar /bin/gtar 
+sudo apt install libxt6
+sudo apt-get install libcurl4-openssl-dev libssl-dev
+in R
+install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest'))
+devtools::install_github('IRkernel/IRkernel')
+# 只在當前使用者下安裝
+IRkernel::installspec()
+# 或者是在系統下安裝
+IRkernel::installspec(user = FALSE)
 
+
+
+------------------------------------------------------
 apt install r-base
 
 # example https://www.digitalocean.com/community/tutorials/how-to-install-r-packages-using-devtools-on-ubuntu-16-04
