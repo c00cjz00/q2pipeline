1527d3d5ba7381c1b7ade0a0e210404c784fa226
c00cjz00 <summerhill001@gmail.com>
2019-12-22 11:14:14 +0800

    Update install_jupyter_for_R.sh

--- install_jupyter_for_R.sh
+++ install_jupyter_for_R.sh
@@ -1,4 +1,13 @@
+
+
 apt install r-base
+
+# example https://www.digitalocean.com/community/tutorials/how-to-install-r-packages-using-devtools-on-ubuntu-16-04
+# sudo apt-get install build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev
+# sudo -i R
+# install.packages('devtools')
+# sudo apt-get install -y r-cran-latticeextra
+
 pip3 install –upgrade pip
 sudo apt-get install libzmq3-dev libcurl4 libcurl4-openssl-dev
 sudo apt-get install build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev
